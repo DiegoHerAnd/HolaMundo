2a1b99940df0d987627cbfdeac891e20f8304556
kevin <kevin@DESKTOP-PABP6JI>
2025-04-03 17:50:47 +0200

    commit 2

--- src/HolaMundo/Programa.java
+++ src/HolaMundo/Programa.java
@@ -4,7 +4,7 @@ public class Programa {
 
 	public static void main(String[] args) {	
 		System.out.println("Hola mundo!");
-		System.out.print("2. Modificado por kevin");
+		System.out.print("Modificado por kevin");
 	}
 
 }
