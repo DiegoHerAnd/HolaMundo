40872a5c99803d43ff896eea1b91fad03a536a7b
diego <diegoherand06@gmail.com>
2025-04-03 18:53:25 +0200

    commit 3

--- src/HolaMundo/Programa.java
+++ src/HolaMundo/Programa.java
@@ -5,6 +5,7 @@ public class Programa {
 	public static void main(String[] args) {	
 		System.out.println("Hola mundo!");
 		System.out.print("Modificado por kevin");
+		System.out.println("Ultimo commit de Diego");
 	}
 
 }
